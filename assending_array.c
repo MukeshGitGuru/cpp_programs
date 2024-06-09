9bc1817a386ba554b0ff41c9a04ed1bd3f479fbb
MukeshGitGuru <mukeshmukupandey@gmail.com>
2024-06-09 15:12:40 +0530

    assensding_array problem added

---
+++ assending_array.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+	int size,i,j,temp;
+	
+	printf("Enter array size\n");
+	scanf("%d",&size);
+
+	int arr[size];
+
+	printf("Enter array elements\n");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+
+	printf("Array elements\n");
+
+	for(i=0;i<size;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+
+
+	for(i=0;i<size;i++)
+	{
+		for(j=i+1;j<size;j++)
+		{
+			if(arr[i] > arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	
+	printf("assending order of array \n");
+
+	for(i=0;i<size;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
